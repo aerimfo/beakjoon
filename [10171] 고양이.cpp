fc6652524675f74e2103389b80b346d8ea05805a
KIM YERIM <52696359+aerimfo@users.noreply.github.com>
2020-06-20 13:50:51 +0900

    Add files via upload

---
+++ [10171] 고양이.cpp
@@ -0,0 +1,12 @@
+// [10171] 고양이
+// https://www.acmicpc.net/problem/10171
+
+#include <cstdio>
+
+int main(void)
+{
+    printf("\\    /\\\n");
+    printf(" )  ( ')\n");
+    printf("(  /  )\n");
+    printf(" \\(__)|");
+}
