f326ec5e0e10bbc9559c058f8aa28ea71f037dc3
aerim <aerimforest98@gmail.com>
2021-06-26 03:50:32 +0900

    dp

--- [1463] 1로 만들기.cpp
+++ [1463] 1로 만들기.cpp
@@ -8,28 +8,23 @@ using namespace std;
 
 int dp[MAX] = {0}; // 최소 연산 횟수
 
-void minCal(int x)
-{
-    dp[x] = dp[x-1] + 1;
-
-    if(x % 2 == 0) {
-        dp[x] = dp[x] > dp[x/2] + 1 ? dp[x/2] + 1 : dp[x];
-    }
-
-    if(x % 3 == 0) {
-        dp[x] = dp[x] > dp[x/3] + 1 ? dp[x/3] + 1 : dp[x];
-    }
-}
-
 int main(void)
 {
     int n;
     cin >> n;
 
-    dp[0] = 0, dp[1] = 0, dp[2] = 1, dp[3] = 1;
+    dp[0] = 0, dp[1] = 0;
 
-    for(int i = 4 ; i <= n ; i++) {
-        minCal(i);
+    for(int i = 2 ; i <= n ; i++) {
+        dp[i] = dp[i-1] + 1;
+ 
+        if(i % 2 == 0) {
+            dp[i] = min(dp[i], dp[i/2] + 1);
+        }
+
+        if(i % 3 == 0) {
+            dp[i] = min(dp[i], dp[i/3] + 1);
+        }
     }
 
     cout << dp[n] << '\n';
