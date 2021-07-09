6fe2ce76841f0fbce05654779e5a8bea468977bd
aerim <aerimforest98@gmail.com>
2021-07-09 16:06:03 +0900

    dp

---
+++ [9095] 1, 2, 3 더하기(2).cpp
@@ -0,0 +1,30 @@
+// [1, 2, 3 더하기]
+// https://www.acmicpc.net/problem/9095
+
+
+#include <iostream>
+using namespace std;
+
+int dp[11] { 0, 1, 2, 4, };
+
+int go(int n) 
+{
+    if(dp[n] != 0) return dp[n];
+    
+    dp[n] += go(n-3);
+    dp[n] += go(n-2);
+    dp[n] += go(n-1);
+    
+    return dp[n];
+}
+
+int main(void)
+{
+    int t, n;
+    cin >> t;
+
+    while(t--) {
+        cin >> n;
+        cout << go(n) << '\n';
+    }
+}
