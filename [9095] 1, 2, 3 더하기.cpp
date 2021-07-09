bffbc54d002a1c91b9d5183b2d9d54c75a61bf1e
aerim <aerimforest98@gmail.com>
2021-07-09 16:11:29 +0900

    dp

--- [9095] 1, 2, 3 더하기.cpp
+++ [9095] 1, 2, 3 더하기.cpp
@@ -1,19 +1,22 @@
-/*[9095] 1, 2, 3 ¥ı«œ±‚*/
-/*https://www.acmicpc.net/problem/9095 */
+// [9095] 1, 2, 3 ÎçîÌïòÍ∏∞ 
+// https://www.acmicpc.net/problem/9095
+// dp(Bottom-up)
 
 #include <iostream>
 using namespace std;
 
 int main(void)
 {
-	int t, n; /*the number of test cases*/
+	int t, n; // the number of test cases
 	int arr[12] = { 0, 1, 2, 4, };
 	cin >> t;
+
 	for (int i = 0; i < t; i++)
 	{
 		cin >> n;
-		for (int j = 4; j <= n; j++)
+		for (int j = 4; j <= n; j++) {
 			arr[j] = arr[j - 1] + arr[j - 2] + arr[j - 3];
+		}
 		cout << arr[n] << endl;
 	}
 }
