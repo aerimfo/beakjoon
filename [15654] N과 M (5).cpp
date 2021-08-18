78a1df3954e05f616581b44cd95dfe2d7363ac1c
aerim <aerimforest98@gmail.com>
2021-08-18 18:30:09 +0900

    백트래킹

---
+++ [15654] N과 M (5).cpp
@@ -0,0 +1,47 @@
+// [15654] N과 M (5)
+// https://www.acmicpc.net/problem/15654
+// 백트래킹
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int arr[10];
+bool visited[10];
+vector <int> num;
+
+// cnt = 현재까지 선택한 숫자의 개수
+void dfs(int cnt)
+{
+    // m개를 모두 선택했다면
+    if(cnt == m) {
+        for(int i = 0 ; i < m ; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+
+    for(int i = 0 ; i < n ; i++) {
+        if(visited[i] == false) { // 선택한 적이 없으면
+            visited[i] = true; // 선택
+            arr[cnt] = num[i]; // 선택한 숫자 저장
+            dfs(cnt + 1);
+            visited[i] = false;
+        }
+    }
+}
+
+int main(void)
+{
+    int tmp;
+    cin >> n >> m;
+    for(int i = 0 ; i < n ; i++) {
+        cin >> tmp;
+        num.push_back(tmp);
+    }
+    sort(num.begin(), num.end());
+    dfs(0);
+}
