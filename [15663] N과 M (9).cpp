804eb2e6663c5d66665ddbc4419c73cafdd36e1a
aerim <aerimforest98@gmail.com>
2021-08-18 20:53:57 +0900

    백트래킹

--- [15663] N과 M (9).cpp
+++ [15663] N과 M (9).cpp
@@ -1,20 +1,24 @@
-// [15657] N과 M (8)
-// https://www.acmicpc.net/problem/15657
+// [15663] N과 M (9)
+// https://www.acmicpc.net/problem/15663
 // 백트래킹
 
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cstdio>
 using namespace std;
 
-int n, m;
-int arr[10];
-bool visited[10];
+int n, m, pre = 0;
+int arr[8];
+bool visited[8];
 vector <int> num;
 
 // cnt = 현재까지 선택한 숫자의 개수
 void dfs(int cnt)
 {
+    // pre = 0; -> 답 제대로 안나옴. 왜..?
+    int pre = 0; // 7 9 -> 9 1 가능하도록
+
     // m개를 모두 선택했다면
     if(cnt == m) {
         for(int i = 0 ; i < m ; i++) {
@@ -25,9 +29,12 @@ void dfs(int cnt)
     }
 
     for(int i = 0 ; i < n ; i++) {
-        if(arr[cnt - 1] <= num[i]) { // 직전에 선택한 수보다 크거나 같다면
+        if(visited[i] == false && num[i] != pre) {
+            visited[i] = true; // 선택
             arr[cnt] = num[i]; // 선택한 숫자 저장
+            pre = num[i];
             dfs(cnt + 1);
+            visited[i] = false;
         }
     }
 }
