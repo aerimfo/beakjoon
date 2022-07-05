4b28b7dbad68b512229fa46baac8abe3f9a8c8f5
aerimforest <aerimforest98@gmail.com>
2022-07-05 17:17:44 +0900

    [Algo] 정렬

--- [18870] 좌표 압축.cpp
+++ [18870] 좌표 압축.cpp
@@ -1,34 +1,34 @@
 // [18870] 좌표 압축
-// https://www.acmicpc.net/problem/18870
-
+// 정렬
 #include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
 int n;
-vector <int> info, ans;
+vector <int> x, ans;
 
 void input()
 {
-    int x;
+    int num;
     cin >> n;
     for(int i = 0; i < n; i++) {
-        cin >> x;
-        info.push_back(x);
+        cin >> num;
+        x.push_back(num);
     }
 }
 
-void solve()
+void solution()
 {
-    vector <int> temp(info);
-    sort(temp.begin(), temp.end());
-    temp.erase(unique(temp.begin(), temp.end()), temp.end());
+    vector<int> tmp(x);
+    sort(tmp.begin(), tmp.end());
+    tmp.erase(unique(tmp.begin(), tmp.end()), tmp.end());
 
     for(int i = 0; i < n; i++) {
-        int idx = lower_bound(temp.begin(), temp.end(), info[i]) - temp.begin();
+        int idx = lower_bound(tmp.begin(), tmp.end(), x[i]) - tmp.begin();
         cout << idx << ' ';
     }
+    cout << '\n';
 }
 
 int main(void)
@@ -37,7 +37,6 @@ int main(void)
     cin.tie(NULL); cout.tie(NULL);
 
     input();
-    solve();
-    
+    solution();
     return 0;
 }
